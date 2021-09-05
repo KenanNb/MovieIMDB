276835602c65958dca745ac6c75a5c940ab849b8
Kenan <kenan.bagiramov@mail.ru>
2021-09-05 17:57:43 +0400

    Third commit

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace WpfApp5
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int Count { get; set; } = 0;
+        public string Rating { get; set; }
         public string ImagePath { get; set; }
         public string Minute { get; set; }
         public string Description { get; set; }
@@ -56,6 +56,100 @@ namespace WpfApp5
             Description = SingleData.Genre;
 
             movieLabel.Content = Minute + " " + Description;
+            Rating = SingleData.imdbRating;
+            double rating = double.Parse(Rating);
+            if(rating != 0)
+            {
+                star0.Visibility = Visibility.Collapsed;
+                star01.Visibility = Visibility.Collapsed;
+                star02.Visibility = Visibility.Collapsed;
+                star03.Visibility = Visibility.Collapsed;
+                star04.Visibility = Visibility.Collapsed;
+            }
+            if(rating > 0.0 && rating < 2.0)
+            {
+                starHalf.Visibility = Visibility.Visible;
+            }
+            if(rating == 2)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+            }
+            if (rating > 2.0 && rating < 4.0)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Visible;
+            }
+            if (rating == 4)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+            }
+            if (rating > 4.0 && rating < 6.0)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf2.Visibility = Visibility.Visible;
+            }
+            if (rating == 6)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf2.Visibility = Visibility.Collapsed;
+                
+            }
+            if (rating > 6.0 && rating < 8.0)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf2.Visibility = Visibility.Collapsed;
+                star3.Visibility = Visibility.Visible;
+                starHalf3.Visibility = Visibility.Visible;
+            }
+            if (rating == 8)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf3.Visibility = Visibility.Collapsed;
+                star4.Visibility = Visibility.Visible;
+            }
+            if (rating > 8.0 && rating < 10.0)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf2.Visibility = Visibility.Collapsed;
+                star3.Visibility = Visibility.Visible;
+                starHalf3.Visibility = Visibility.Collapsed;
+                star4.Visibility = Visibility.Visible;
+                starHalf4.Visibility = Visibility.Visible;
+            }
+            if (rating == 10)
+            {
+                starHalf.Visibility = Visibility.Collapsed;
+                star1.Visibility = Visibility.Visible;
+                starHalf1.Visibility = Visibility.Collapsed;
+                star2.Visibility = Visibility.Visible;
+                starHalf3.Visibility = Visibility.Collapsed;
+                star4.Visibility = Visibility.Visible;
+                starHalf4.Visibility = Visibility.Collapsed;
+                star5.Visibility = Visibility.Visible;
+            }            
+            RatingLbl1.Content = "Rating";
+            RatingLbl2.Content = Rating;
+            PlotTxtblock.Text = SingleData.Plot;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -72,15 +166,22 @@ namespace WpfApp5
             {
               Index--;
               GetMovie();
-
             }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            preBtn.Visibility = Visibility.Visible;
-            Index++;
-            GetMovie();
+            try
+            {
+              preBtn.Visibility = Visibility.Visible;
+              Index++;
+              GetMovie();             
+            }
+            catch (Exception)
+            {
+                nextBtn.Visibility = Visibility.Hidden;
+                --Index;
+            }
         }
     }
 }
